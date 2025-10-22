866e3577fb7e8a3a1fae301ae56bba65879311e0
Uxue Mancisidor laskurain <uxmancis@c4r6s1.42urduliz.com>
2025-10-22 18:55:55 +0200

    ex01 was corrected according to subject, ex02: explanatory comments were added to exercise

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uxuemancisidorlaskurain <uxuemancisidor    +#+  +:+       +#+        */
+/*   By: uxmancis <uxmancis>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:22:51 by uxmancis          #+#    #+#             */
-/*   Updated: 2025/10/21 12:32:55 by uxuemancisi      ###   ########.fr       */
+/*   Updated: 2025/10/22 18:54:25 by uxmancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,28 +42,70 @@
 
 // #endif
 
-#ifndef MutantStack_HPP
-# define MutantStack_HPP
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
 #include <stack> //std::stack container
+
+/* std::stack is not a container itself, it's a container adapter. It's
+*  actually buiñt on top of another container (std::deque<T> by default) + 
+*  restricts access to behave like a stack (LIFO).
+*
+*  std::stack 
+*/
 
 template	<typename T>
 class	MutantStack : public std::stack<T>
 {
 	public:
+
+		/* When we use a template class, the full implementation must be in .hpp */
+		
 		/*OCF*/
-		MutantStack(): std::stack<T>() {};
-		MutantStack(const MutantStack& to_copy): std::stack<T>(static_cast<std::stack<T> > (to_copy)) {};
-		MutantStack& operator=(const MutantStack& to_asign) {static_cast<std::stack<T> > (this) = static_cast<std::stack<T> > (to_asign);};
-		~MutantStack() {};
+		MutantStack(): std::stack<T>(){}; /* Default Constructor */
+		MutantStack(const MutantStack	&src2cpy): std::stack<T>(std::stack<T> (src2cpy)){}; /* Copy constructor */
+		MutantStack& operator=(const MutantStack	&src2cpy)/* Copy assignment operator */
+		{
+			static_cast<std::stack<T> > (this) = static_cast<std::stack<T> > (src2cpy);
+		}; 
+		~MutantStack(){}; /* Destructor */
 
 	public:
+		/* About iterators: 
+		*		#1 std --> standard Namespace in C++
+		*			└── #2 ::stack<T> --> The base template Class, instanced with type T
+		* 				└── #3 :: container_type --> an alias (typedef) inside class std::stack, typedef Container container_type
+		*					 └── #4 ::iterator --> 
+		*		Summary: 'iterator' named type, that exists in 'container_type' named type that uses stack<T>
+		*
+		*  About syntax: 
+		*		typedef --> creates an alias (nickname) for a type
+		*				E.g.: 
+		*					typedef unsigned long long ULL;
+		*					ULL age = 42;
+		*						, instead of
+		*					unsigned long long age = 42;
+		*
+		*				E.g.2:
+		*					typedef (...) iterator; --> Example 
+		*					typedef (real type) alias; --> Explanation
+		*
+		*		typename --> because of template, T, it must go before std::stack in this case
+		*
+		*		:: --> it's used to access a member, type or method that is defined inside another entity (namespace or class)
+		*/
 		typedef typename std::stack<T>::container_type::iterator iterator;
 		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-		iterator begin() { return std::stack<T>::c.begin();}
-		iterator end() { return std::stack<T>::c.end();}
-		const_iterator begin() const { return std::stack<T>::c.begin();}
-		const_iterator end() const { return std::stack<T>::c.end();}
+		
+		/* begin() and end() named methods return 'iterator' and 'const_iterator' types */
+		iterator begin(){
+			return std::stack<T>::c.begin();}
+		iterator end(){
+			return std::stack<T>::c.end();}
+		const_iterator begin() const{
+			return std::stack<T>::c.begin();}
+		const_iterator end() const {
+			return std::stack<T>::c.end();}
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: uxuemancisidorlaskurain <uxuemancisidor    +#+  +:+       +#+        */
+/*   By: uxmancis <uxmancis>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:18:09 by uxmancis          #+#    #+#             */
-/*   Updated: 2025/10/21 13:51:36 by uxuemancisi      ###   ########.fr       */
+/*   Updated: 2025/10/22 17:48:24 by uxmancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 #include <iostream>
 /* std::stack container is not iterable*/
 
-/* main required in subject*/
+/* main required in subject: using std::stack methods*/
 // int main()
 // {
 //     MutantStack<int> mstack;
-//     mstack.push(5);
+//     mstack.push(5); //inserts element at the top
 //     mstack.push(17);
-//     std::cout << mstack.top() << std::endl;
-//     mstack.pop();
-//     std::cout << mstack.size() << std::endl;
+//     std::cout << mstack.top() << std::endl; //accesses to the top element, shows 17
+//     mstack.pop(); //removes the top element
+//     std::cout << mstack.size() << std::endl; //shows 1
 //     mstack.push(3);
 //     mstack.push(5);
 //     mstack.push(737);
@@ -40,6 +40,37 @@
 //     std::stack<int> s(mstack);
 //     return 0;
 // }
+
+#include <list>
+
+/* main required in subject: for lists*/
+int main()
+{
+    std::list<int> mstack;
+    mstack.push_back(5); //'push' in our MutantStack (inherites from std::stack)
+    mstack.push_back(17);
+    std::cout << mstack.back() << std::endl; //'top' in our MutantStack (inherites from std::stack)
+    mstack.pop_back(); //'pop' in our MutantStack (inherites from std::stack)
+    std::cout << mstack.size() << std::endl;
+    mstack.push_back(3);
+    mstack.push_back(5);
+    mstack.push_back(737);
+    //[...]
+    mstack.push_back(0);
+
+    /* std::list already has iterators */
+    std::list<int>::iterator it = mstack.begin();
+    std::list<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::list<int> s(mstack);
+    return 0;
+}
 
 #include <stack>
 #define CYAN "\033[36m"
@@ -59,89 +90,89 @@ void putStack(std::stack<int> stack)
 }
 
 /* My tests */
-int main(void)
-{
-    /*********** ORIGINAL METHODS IN STACKS - Compare Stack (Original) vs. our Mutant Stack ***********/
-    std::cout << "\n************************* Show " CYAN "ORIGINAL" RESET_COLOUR" Member functions *************************" << std::endl;
-    /* Test 1: .empty() */
-    std::cout << "📄 Test 1: " CYAN << ".empty()" RESET_COLOUR << std::endl;
-    std::stack<int> stack;
+// int main(void)
+// {
+//     /*********** ORIGINAL METHODS IN STACKS - Compare Stack (Original) vs. our Mutant Stack ***********/
+//     std::cout << "\n************************* Show " CYAN "ORIGINAL" RESET_COLOUR" Member functions *************************" << std::endl;
+//     /* Test 1: .empty() */
+//     std::cout << "📄 Test 1: " CYAN << ".empty()" RESET_COLOUR << std::endl;
+//     std::stack<int> stack;
     
-    if (stack.empty())
-        std::cout << "1) Stack is empty" << std::endl;
-    else 
-        std::cout << "1) Stack is NOT empty" << std::endl;
+//     if (stack.empty())
+//         std::cout << "1) Stack is empty" << std::endl;
+//     else 
+//         std::cout << "1) Stack is NOT empty" << std::endl;
   
-    MutantStack<int> my_mstack;
-    if (my_mstack.empty())
-        std::cout << "2) our Mutant Stack is empty" << std::endl;
-    else 
-        std::cout << "2) our Mutant Stack is NOT empty" << std::endl;
-    std::cout << "✅ Test 1 - COMPLETED Successfully" << std::endl;
+//     MutantStack<int> my_mstack;
+//     if (my_mstack.empty())
+//         std::cout << "2) our Mutant Stack is empty" << std::endl;
+//     else 
+//         std::cout << "2) our Mutant Stack is NOT empty" << std::endl;
+//     std::cout << "✅ Test 1 - COMPLETED Successfully" << std::endl;
 
-    /* Test 2: .size() */
-    std::cout << "\n\n📄 Test 2: " CYAN << ".size()" RESET_COLOUR << std::endl;
-    std::cout << "1) Stack's size is " << stack.size() << std::endl;
-    std::cout << "2) our Mutant Stack's size is " << my_mstack.size() << std::endl;
-    std::cout << "✅ Test 2 - COMPLETED Successfully" << std::endl;
+//     /* Test 2: .size() */
+//     std::cout << "\n\n📄 Test 2: " CYAN << ".size()" RESET_COLOUR << std::endl;
+//     std::cout << "1) Stack's size is " << stack.size() << std::endl;
+//     std::cout << "2) our Mutant Stack's size is " << my_mstack.size() << std::endl;
+//     std::cout << "✅ Test 2 - COMPLETED Successfully" << std::endl;
 
-    /* Test 3: .push(): insert elements into our stack, into TOP of stack */
-    std::cout << "\n\n📄 Test 3: " CYAN << ".push()" RESET_COLOUR ", " CYAN ".top()" RESET_COLOUR ", and " CYAN ".pop() " RESET_COLOUR << std::endl;
-    stack.push(60);
-    stack.push(60);
-    stack.push(60);
-    stack.push(60);
-    std::cout << "1) Original stack: ";
-    putStack(stack);
+//     /* Test 3: .push(): insert elements into our stack, into TOP of stack */
+//     std::cout << "\n\n📄 Test 3: " CYAN << ".push()" RESET_COLOUR ", " CYAN ".top()" RESET_COLOUR ", and " CYAN ".pop() " RESET_COLOUR << std::endl;
+//     stack.push(60);
+//     stack.push(60);
+//     stack.push(60);
+//     stack.push(60);
+//     std::cout << "1) Original stack: ";
+//     putStack(stack);
 
-    my_mstack.push(7);
-    my_mstack.push(7);
-    my_mstack.push(7);
-    std::cout << "2) our Mutant Stack: ";
-    putStack(my_mstack);
-    std::cout << "✅ Test 3 - COMPLETED Successfully" << std::endl;
+//     my_mstack.push(7);
+//     my_mstack.push(7);
+//     my_mstack.push(7);
+//     std::cout << "2) our Mutant Stack: ";
+//     putStack(my_mstack);
+//     std::cout << "✅ Test 3 - COMPLETED Successfully" << std::endl;
 
-    /* Test 4: .swap() */
-    std::cout << "\n\n📄 Test 4: " CYAN << ".swap()" RESET_COLOUR << std::endl;
-    std::cout << "First swap: " << std::endl;
-    stack.swap(my_mstack);
-    std::cout << "1) Original stack: ";
-    putStack(stack);
-    std::cout << "2) our Mutant Stack: ";
-    putStack(my_mstack);
+//     /* Test 4: .swap() */
+//     // std::cout << "\n\n📄 Test 4: " CYAN << ".swap()" RESET_COLOUR << std::endl;
+//     // std::cout << "First swap: " << std::endl;
+//     // stack.swap(my_mstack);
+//     // std::cout << "1) Original stack: ";
+//     // putStack(stack);
+//     // std::cout << "2) our Mutant Stack: ";
+//     // putStack(my_mstack);
 
-    std::cout << "\nSecond swap: " << std::endl;
-    my_mstack.swap(stack);
-    std::cout << "1) Original stack: ";
-    putStack(stack);
-    std::cout << "2) our Mutant Stack: ";
-    putStack(my_mstack);
-    std::cout << "✅ Test 4 - COMPLETED Successfully" << std::endl;
+//     // std::cout << "\nSecond swap: " << std::endl;
+//     // my_mstack.swap(stack);
+//     // std::cout << "1) Original stack: ";
+//     // putStack(stack);
+//     // std::cout << "2) our Mutant Stack: ";
+//     // putStack(my_mstack);
+//     // std::cout << "✅ Test 4 - COMPLETED Successfully" << std::endl;
     
     
-    /*********** SHOW ADDITIONAL METHODS FOR STACKS -  ***********/
-    std::cout << "\n\n************************* Show " YELLOW "ADDITIONAL" RESET_COLOUR " Member functions: ITERATORS *************************" << std::endl;
-    std::cout << "📄 Test 5: " YELLOW ".begin() " RESET_COLOUR " and " YELLOW ".end()" RESET_COLOUR << std::endl;
-    MutantStack<int>::iterator it = my_mstack.begin();
-    MutantStack<int>::iterator ite = my_mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
-    std::cout << *it << std::endl;
-    ++it;
-    }
-    std::cout << "✅ Test 5 - COMPLETED Successfully" << std::endl;
-    return (0);
-}
+//     /*********** SHOW ADDITIONAL METHODS FOR STACKS -  ***********/
+//     std::cout << "\n\n************************* Show " YELLOW "ADDITIONAL" RESET_COLOUR " Member functions: ITERATORS *************************" << std::endl;
+//     std::cout << "📄 Test 5: " YELLOW ".begin() " RESET_COLOUR " and " YELLOW ".end()" RESET_COLOUR << std::endl;
+//     MutantStack<int>::iterator it = my_mstack.begin();
+//     MutantStack<int>::iterator ite = my_mstack.end();
+//     ++it;
+//     --it;
+//     while (it != ite)
+//     {
+//     std::cout << *it << std::endl;
+//     ++it;
+//     }
+//     std::cout << "✅ Test 5 - COMPLETED Successfully" << std::endl;
+//     return (0);
+// }
 
-/* Stack's member functions: 
-*       empty() --> Test whether container is empty
-*       size() --> Return size
-*       top() --> Access (print) next element on top of stack
-*       push() --> Inserts a new element
-*       pop() --> Removes top element
-*       swap() --> Exchanges 100% elements from stacks
-*
-*  Additional member functions: begin() and end() that return iterators
-*/
+// /* Stack's member functions: 
+// *       empty() --> Test whether container is empty
+// *       size() --> Return size
+// *       top() --> Access (print) next element on top of stack
+// *       push() --> Inserts a new element
+// *       pop() --> Removes top element
+// *       swap() --> Exchanges 100% elements from stacks
+// *
+// *  Additional member functions: begin() and end() that return iterators
+// */
